17d757d79c5a883e1fc6c657d99e0c6121dc1467
= <vadosmik@MacBook-Air.local>
2025-12-10 00:15:33 +0100

    create springboot templates

---
+++ checktogo-api/src/test/java/com/vadosmik/checktogo_api/CheckToGoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadosmik.checktogo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckToGoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
